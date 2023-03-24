0ef8b786a1082717f8bedadc7092e9b8b7cfec85
Yaroslav Morozov <teun@tuta.io>
2023-03-24 19:49:13 +0200

    build: add System function

---
+++ StdFunctions/Cstdlib.cs
@@ -0,0 +1,9 @@
+﻿namespace Std;
+
+using System.Runtime.InteropServices;
+
+public static partial class Cstdlib
+{
+    [LibraryImport("msvcrt.dll", StringMarshalling = StringMarshalling.Utf16)]
+    internal static partial int System(string? command);
+}
